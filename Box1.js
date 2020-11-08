d1e1303ceddc002d4cd00160cb58d6f626aaa5bc
harshveer26 <70791946+harshveer26@users.noreply.github.com>
2020-11-08 19:25:41 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,41 @@
+class Box1 {
+    constructor(x, y, width, height){
+        var options = {
+            'friction' : 1.2,
+            'density' : 0.8,
+            //'isStatic' : true
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility = 255;
+
+        World.add(world, this.body);
+    }
+
+    display(){
+        console.log(this.body.speed);
+        if(this.body.speed < 3){
+            var pos = this.body.position;
+            push();
+            fill(255,0,0);
+            stroke("black");
+            strokeWeight(4);
+            translate(pos.x, pos.y);
+            rectMode(CENTER);
+            rect(0, 0, this.width, this.height);
+            pop();
+        } else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+            rect(0, 0, this.width, this.height);
+            pop();
+        }
+    }
+
+        
+    }
+
